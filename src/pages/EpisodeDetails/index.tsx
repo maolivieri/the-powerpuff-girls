183913ec1f4f6de5e7e47a652871a4dd85e0e269
matheus_olivieri <matheus.o@bepozglobal.com>
2021-12-13 17:40:09 -0300

    pages boilerplate

---
+++ src/pages/EpisodeDetails/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const EpisodeDetails: FC = () => {
+  return (
+    <div>
+      <h1>Episode Details</h1>
+    </div>
+  );
+};
---
+++ src/pages/TVShow/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const TVShow: FC = () => {
+  return (
+    <div>
+      <h1>TVShow Page</h1>
+    </div>
+  );
+};
