172f233e66d5615f4938b163aad156b1496290fe
matheus_olivieri <matheus.o@bepozglobal.com>
2021-12-15 17:29:42 -0300

    home page styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,6 +7,10 @@ import {
   SearchButton,
   HighlightTitle,
   ShowBox,
+  Highlights,
+  HighlighImage,
+  HighlighName,
+  Title,
 } from "./style";
 
 import { FiSearch } from "react-icons/fi";
@@ -41,10 +45,26 @@ const Home: FC = () => {
 
   return (
     <Container>
-      <HighlightTitle>Highlight:</HighlightTitle>
-      <Highlight onClick={() => navigate("/show/6771")}>
-        The Powerpuff Girls
-      </Highlight>
+      <HighlightTitle>Highlights:</HighlightTitle>
+      <Highlights>
+        <Highlight onClick={() => navigate("/show/6771")}>
+          <HighlighImage src='https://static.tvmaze.com/uploads/images/medium_portrait/60/151357.jpg' />
+          <HighlighName> The Powerpuff Girls</HighlighName>
+        </Highlight>
+        <Highlight onClick={() => navigate("/show/431")}>
+          <HighlighImage src='https://static.tvmaze.com/uploads/images/medium_portrait/41/104550.jpg' />
+          <HighlighName> Friends</HighlighName>
+        </Highlight>
+        <Highlight onClick={() => navigate("/show/123")}>
+          <HighlighImage src='https://static.tvmaze.com/uploads/images/medium_portrait/0/1389.jpg' />
+          <HighlighName> Lost</HighlighName>
+        </Highlight>
+        <Highlight onClick={() => navigate("/show/66")}>
+          <HighlighImage src='https://static.tvmaze.com/uploads/images/medium_portrait/173/433868.jpg' />
+          <HighlighName> The Big Bang Theory</HighlighName>
+        </Highlight>
+      </Highlights>
+      <Title>Search for your favourite shows</Title>
       <SearchWrapper onSubmit={handleSubmit}>
         <SearchBox
           onChange={(event) => setSearch(event.target.value)}
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -17,27 +17,63 @@ export const Container = styled.div`
 
 export const Highlight = styled.div`
   margin: 0.2rem 2rem 2rem 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
   flex: 1;
   cursor: pointer;
 `;
+
+export const Highlights = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+`;
+
 export const HighlightTitle = styled.h3`
   text-align: center;
   flex: 1;
-  width: 100%;
+  margin-bottom: 1rem;
+`;
+
+export const Title = styled.h3`
+  text-align: center;
+  flex: 1;
+  margin-bottom: 1rem;
+`;
+
+export const HighlighName = styled.p`
+  font-size: 0.8rem;
+  font-weight: 700;
+  text-align: center;
+`;
+
+export const HighlighImage = styled.img`
+  width: 80%;
+  margin-bottom: 0.3rem;
+  flex: 1;
 `;
 
 export const SearchWrapper = styled.form`
+  position: relative;
   display: flex;
   align-items: center;
 `;
 
 export const SearchBox = styled.input`
+  display: flex;
+  align-items: center;
+  border-radius: 30px;
+  padding: 0.3rem 0.8rem;
+  border: none;
   flex: 1;
   height: 2rem;
+  box-shadow: 0px 5px 8px rgba(15, 15, 15, 0.15);
 `;
 
 export const SearchButton = styled.button`
+  position: absolute;
+  right: 0.3rem;
   outline: none;
   text-decoration: none;
   display: flex;
@@ -45,11 +81,12 @@ export const SearchButton = styled.button`
   justify-content: center;
   background: rgba(15, 15, 15, 0.8);
   border: none;
-  height: 2.3rem;
-  width: 2.3rem;
+  height: 2rem;
+  width: 2rem;
   margin-left: -1px;
   color: #fff;
   cursor: pointer;
+  border-radius: 9999px;
 
   &:hover {
     background: rgba(15, 15, 15, 0.5);
@@ -64,6 +101,7 @@ export const ShowBox = styled.div`
   background: rgba(200, 200, 200, 0);
   padding: 0.5rem 0.8rem;
   cursor: pointer;
+  border-radius: 30px;
 
   &:hover {
     background: rgba(200, 200, 200, 0.2);
